8699d4cb64c00bac089d1f7ca50b6d93ec13e308
Rashedul Islam Sagor <sagor.touch@gmail.com>
2020-09-22 21:51:11 +0600

    User seeder class has been created and static 3 user seed has been added

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,54 @@
+<?php
+
+use App\User;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $facker)
+    {
+        User::insert($this->staticUsers($facker));
+    }
+
+    /**
+     * Generate Static User
+     *
+     * @param $faker
+     * @return array
+     */
+    public function staticUsers($faker): array
+    {
+        $commonAttributes = [
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'created_at' => now(),
+            'updated_at' => now(),
+        ];
+
+
+        return [
+            array_merge($commonAttributes, [
+                'name' => 'Super Admin',
+                'email' => 'adminh@example.com',
+                'remember_token' => Str::random(10),
+            ]),
+            array_merge($commonAttributes, [
+                'name' => 'jon',
+                'email' => 'jon@example.com',
+                'remember_token' => Str::random(10),
+            ]),
+            array_merge($commonAttributes, [
+                'name' => 'Joly',
+                'email' => 'joly@example.com',
+                'remember_token' => Str::random(10),
+            ]),
+        ];
+    }
+}
