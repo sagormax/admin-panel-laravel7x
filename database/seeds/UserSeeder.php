e72befae4190add130fd0e954720147759a65761
Rashedul Islam Sagor <sagor.touch@gmail.com>
2020-09-23 09:42:15 +0600

    [APL-5] #comment Added a attributes comment

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -25,6 +25,7 @@ class UserSeeder extends Seeder
      */
     public function staticUsers($faker): array
     {
+        // User common attributes
         $commonAttributes = [
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
